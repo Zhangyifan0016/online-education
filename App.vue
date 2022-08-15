b17ed6cdb5e3d97c35233fc87dcb2dbd90421186
Zhangyifan0016 <z351072259@163.com>
2022-08-15 18:39:35 +0800

    feat(新功能):全局样式与全局配置

--- App.vue
+++ App.vue
@@ -14,4 +14,9 @@
 
 <style>
 	/*每个页面公共css */
+	@import url("./common/free.css");
+	@import url("./common/common.css");
+	@import url("./common/animate.min.css");
+	@import url("@/common/iconfont/iconfont.css");
+
 </style>
