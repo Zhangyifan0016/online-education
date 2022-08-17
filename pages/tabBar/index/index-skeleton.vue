31721f920976b0cee5f156ffc8248b40801047e4
Zhangyifan0016 <z351072259@163.com>
2022-08-17 16:07:46 +0800

    feat(新功能)：新增骨架屏加载优化功能

---
+++ pages/tabBar/index/index-skeleton.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<view class="p-2">
+			<i-skeleton width="710rpx" height="75rpx" oClass="mb-2 rounded"></i-skeleton>
+			<i-skeleton width="710rpx" height="300rpx" oClass="rounded"></i-skeleton>
+		</view>
+		<view class="flex flex-wrap py-2">
+			<view class="flex flex-column align-center justify-center" style="width: 25%;" v-for="i in 8" :key="i">
+				<i-skeleton width="75rpx" height="75rpx" circle oClass="mb-2"></i-skeleton>
+				<i-skeleton width="75rpx" height="30rpx" oClass="mb-2"></i-skeleton>
+			</view>
+		</view>
+		<view class="flex flex-wrap py-2">
+			<view class="flex align-center justify-center" style="width: 50%;">
+				<i-skeleton width="354rpx" height="130rpx" oClass="mb-2"></i-skeleton>
+			</view>
+			<view class="flex align-center justify-center" style="width: 50%;">
+				<i-skeleton width="354rpx" height="130rpx" oClass="mb-2"></i-skeleton>
+			</view>
+		</view>
+		<view class="p-2">
+			<i-skeleton width="70rpx" height="35rpx"></i-skeleton>
+		</view>
+		<view class="flex p-2">
+			<i-skeleton width="300rpx" height="170rpx"></i-skeleton>
+			<view class="ml-2">
+				<i-skeleton width="364rpx" height="38rpx" oClass="mb-2"></i-skeleton>
+				<i-skeleton width="120rpx" height="25rpx" oClass="mb-2"></i-skeleton>
+				<i-skeleton width="170rpx" height="43rpx"></i-skeleton>
+			</view>
+		</view>
+	</view>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
--- pages/tabBar/index/index.vue
+++ pages/tabBar/index/index.vue
@@ -1,66 +1,84 @@
 <template>
-	<view class="animate__animated animate__fadeIn animate__fast">
-		<block v-for="(item,index) in indexData" :key="index">
-			<i-search-bar v-if="item.type==='search'" :placeholder="item.placeholder"></i-search-bar>
-			<i-swiper v-if="item.type==='swiper'" :data="item.data"></i-swiper>
-			<i-icons v-if="item.type==='icons'" :data="item.data"></i-icons>
-			<i-coupon v-if="item.type==='coupon'" :data="couponData"></i-coupon>
-			<!-- 拼团 // 秒杀 -->
-			<view v-if="item.type === 'promotion'">
-				<i-active-list :type="item.listType" :data="activityData">
-					<template v-slot:divider>
-						<view class="divider"></view>
-					</template>
-				</i-active-list>
-			</view>
-			<!-- 最新列表 -->
-			<view v-if="item.type==='list'">
-				<view class="divider"></view>
-				<i-course-title :item="item"></i-course-title>
-				<view>
-					<i-course-list :type="item.listType" v-for="(childItem,index) in item.data" :key="index" :item="childItem"></i-course-list>
+	<view>
+		<index-skeleton v-if="!loadingStatus"></index-skeleton>
+
+		<view v-else class="animate__animated animate__fadeIn animate__fast">
+			<block v-for="(item,index) in indexData" :key="index">
+				<i-search-bar v-if="item.type==='search'" :placeholder="item.placeholder"></i-search-bar>
+				<i-swiper v-if="item.type==='swiper'" :data="item.data"></i-swiper>
+				<i-icons v-if="item.type==='icons'" :data="item.data"></i-icons>
+				<i-coupon v-if="item.type==='coupon'" :data="couponData"></i-coupon>
+				<!-- 拼团 // 秒杀 -->
+				<view v-if="item.type === 'promotion'">
+					<i-active-list :type="item.listType" :data="activityData">
+						<template v-slot:divider>
+							<view class="divider"></view>
+						</template>
+					</i-active-list>
 				</view>
-			</view>
-			<!-- 底部图片 -->
-			<view v-if="item.type==='imageAd'">
-				<view class="divider"></view>
-				<image :src="item.data" mode="aspectFill" style="width: 750rpx;height: 360rpx;"></image>
-			</view>
-		</block>
+				<!-- 最新列表 -->
+				<view v-if="item.type==='list'">
+					<view class="divider"></view>
+					<i-course-title :item="item"></i-course-title>
+					<view>
+						<i-course-list :type="item.listType" v-for="(childItem,index) in item.data" :key="index"
+							:item="childItem"></i-course-list>
+					</view>
+				</view>
+				<!-- 底部图片 -->
+				<view v-if="item.type==='imageAd'">
+					<view class="divider"></view>
+					<image :src="item.data" mode="aspectFill" style="width: 750rpx;height: 360rpx;"></image>
+				</view>
+			</block>
+		</view>
 	</view>
 </template>
 
 <script>
 	import IndexModel from "@/model/indexModel"
+	import indexSkeleton from './index-skeleton'
 	export default {
+		components: {
+			indexSkeleton
+		},
 		data() {
 			return {
-				indexData:[],
-				activityData:[],
-				couponData:[],
+				loadingStatus: false,
+				indexData: [],
+				activityData: [],
+				couponData: [],
 				query: {
 					page: 1,
 					usable: 1
 				}
 			};
 		},
-		async onLoad() {
-			await this.getIndexData()
-			await this.getCoupon()
-			await this.getActivity()
+		onLoad() {
+			this.loadRequest()
 		},
 		methods: {
+			async loadRequest() {
+				try {
+					await this.getIndexData()
+					await this.getCoupon()
+					await this.getActivity()
+					this.loadingStatus = true
+				} catch (e) {
+					//TODO handle the exception
+				}
+			},
 			async getIndexData() {
 				const response = await IndexModel.getIndexData()
-				this.indexData=response
+				this.indexData = response
 			},
-			async getCoupon(){
-				const response=await IndexModel.getCoupon()
-				this.couponData=response
+			async getCoupon() {
+				const response = await IndexModel.getCoupon()
+				this.couponData = response
 			},
 			async getActivity() {
-				const type = this.indexData.filter(item=> item.type === 'promotion')[0].listType
-				const response = await IndexModel.getActivity(type,this.query)
+				const type = this.indexData.filter(item => item.type === 'promotion')[0].listType
+				const response = await IndexModel.getActivity(type, this.query)
 				this.activityData = response.rows
 			},
 		},
